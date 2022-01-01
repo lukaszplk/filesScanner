ea2cc4b9d11a07a08f9dc872bea76592ac3b6621
lukaszplk <86591130+lukaszplk@users.noreply.github.com>
2022-01-01 14:18:17 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+from os import walk
+import os
+import pickle
+
+
+try:
+    with open("save.pkl", "rb") as file:
+        files_list = pickle.load(file)
+except FileNotFoundError:
+    files_list = []
+
+flag=True
+for path, _, names in walk('F:\holandia'):
+    for i in range(len(names)):
+        if names[i] in files_list:
+            continue
+        else:
+            print("Nowy plik: ", end='')
+            files_list.append(names[i])
+            print(names[i], " w ", path)
+            flag = False
+
+with open('save.pkl', 'wb') as file:
+    pickle.dump(files_list, file)
+
+if flag:
+    print("Brak nowych plików.")
+   
